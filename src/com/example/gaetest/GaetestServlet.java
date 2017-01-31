362f9fafd23a6ed4ae1570b8c0659a6bb3f1ed17
yoshinori <yoshinori@192.168.152.177>
2017-01-31 22:01:53 +0900

    bisyuusei

--- src/com/example/gaetest/GaetestServlet.java
+++ src/com/example/gaetest/GaetestServlet.java
@@ -8,6 +8,6 @@ public class GaetestServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
-		resp.getWriter().println("piyopiyo!");
+		resp.getWriter().println("piyopiyo!!!!!");
 	}
 }
